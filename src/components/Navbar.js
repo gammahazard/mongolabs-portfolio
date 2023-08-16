d366f4c200e4f74f77ec282aed4fe84a7230aad3
gammahazard <92896466+gammahazard@users.noreply.github.com>
2023-08-16 00:23:32 -0400

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <nav className="navbar">
             <div className="navbar-logo">MongoLabs</div>
-            <div className="navbar-links">
-                <Link to="/">Work</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
+            <div className="menu-section">
+                <div className="burger-menu" onClick={() => setIsOpen(!isOpen)}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
+                    <Link to="/">Work</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
+                </div>
             </div>
         </nav>
     );
 }
 
-export default Navbar;
+
+export default Navbar
